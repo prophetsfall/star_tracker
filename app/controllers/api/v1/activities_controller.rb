bdc45a8c2e00e580fa153f28d93cde4cb2a35a73
Patrick Whelan <patrickawhelan@gmail.com>
2019-10-11 15:49:22 -0400

    pushing straight to master WOAH!

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -1,18 +1,5 @@
 class Api::V1::ActivitiesController < ApplicationController
   def index
-    render json: {
-      activities: [
-        {id:1,name: "cardio"},
-        {id:2,name: "weights"},
-        {id:3,name: "dancing"},
-        {id:34,name: "dancing"},
-        {id:35,name: "dancing"},
-        {id:36,name: "dancing"},
-        {id:37,name: "dancing"},
-        {id:38,name: "dancing"},
-        {id:39,name: "dancing"},
-        {id:4,name: "cuddling"}
-      ]
-    }
+    @activities = Activity.all
   end
 end
